11e3a5171a1d014be007fabb8cc7f27800744b18
SD056737 <chinmaya.datta@cerner.com>
2021-04-03 16:30:31 +0530

    Added Spring-Data-Rest properties and configurations

--- src/main/java/com/datta/MySpringApplication/SpringDataJPADAO/EmployeeRepository.java
+++ src/main/java/com/datta/MySpringApplication/SpringDataJPADAO/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.datta.MySpringApplication.SpringDataJPADAO;
 import com.datta.MySpringApplication.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    //http://localhost:3307/chinmaya-api/members
     //Here we no need to write any code, thats the magic of spring data JPA
     // Spring data JPA will provide all CRUD operations by default
     //we just need to pass the entity name and primary key id
